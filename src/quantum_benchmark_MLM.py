59e570ab92d18f5597a3a6be40ad4f77b0548553
mazinnadaf <mazinnadaf@gmail.com>
2026-02-10 18:34:27 -0500

    3

--- src/quantum_benchmark_MLM.py
+++ src/quantum_benchmark_MLM.py
@@ -17,7 +17,7 @@ from custom_bert_lastlayer_attention import CustomBertForMaskedLM_LastLayerAtten
 # CONFIGURATION
 # ==============================================================================
 N_FOLDS = 5
-START_FOLD = 1   
+START_FOLD = 3   
 EPOCHS = 3
 LEARNING_RATE = 1e-5
 BATCH_SIZE = 8  
